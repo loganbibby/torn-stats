c18f3f60a16edbdc778d4c87603e7185c5d4a5e9
Logan Bibby <logan@loganbibby.com>
2022-12-04 16:06:28 -0600

    Updates

--- torn_stats/client.py
+++ torn_stats/client.py
@@ -6,6 +6,25 @@ class LogTypes(IntEnum):
     XANAX = 2290
     MISSION = 7815
     CRIME = 5725
+    TRAVEL = 6000
+    GYM_DEFENSE = 5301
+    GYM_STRENGTH = 5300
+    GYM_DEXTERITY = 5303
+    GYM_SPEED = 5302
+    ATTACK_MUG = 8155
+    ATTACK_HOSPITALIZE = 8160
+    ATTACK_LEAVE = 8150
+    VAULT_DEPOSIT = 5850
+    VAULT_WITHDRAWAL = 5851
+    TRAVEL_BUY = 4201
+    COMPANY_PAY = 6221
+    COMPANY_TRAIN = 6264
+    MARKET_SELL = 1104
+
+
+class LogCategories(IntEnum):
+    MONEY_INCOMING = 17
+    MONEY_OUTGOING = 14
 
 
 class TornClient(object):
@@ -34,7 +53,7 @@ class TornClient(object):
             "user", "basic"
         )
 
-    def get_logs(self, log_type=None, start_date=None, end_date=None, **kwargs):
+    def get_logs(self, log_type=None, log_category=None, start_date=None, end_date=None, **kwargs):
         if start_date:
             kwargs["from"] = int(round(start_date.timestamp()))
 
@@ -42,7 +61,16 @@ class TornClient(object):
             kwargs["to"] = int(round(end_date.timestamp()))
 
         if log_type:
-            kwargs["log"] = int(log_type)
+            if isinstance(log_type, list):
+                kwargs["log"] = ",".join([str(t.value) for t in log_type])
+            else:
+                kwargs["log"] = int(log_type)
+
+        if log_category:
+            if isinstance(log_category, list):
+                kwargs["cat"] = ",".join([str(t.value) for t in log_category])
+            else:
+                kwargs["cat"] = int(log_category)
 
         payload = self.execute(
             "user", "log",
@@ -53,3 +81,63 @@ class TornClient(object):
             return []
 
         return [v for k, v in payload.items()]
+
+    def get_total_travel_time(self, start_date=None, end_date=None):
+        logs = self.get_logs(
+            LogTypes.TRAVEL,
+            start_date=start_date,
+            end_date=end_date
+        )
+
+        return round(sum([l["data"]["duration"] for l in logs]) / 60, 0)
+
+    def get_money_received(self, **kwargs):
+        logs = self.get_logs(
+            log_category=LogCategories.MONEY_INCOMING,
+            **kwargs
+        )
+
+        money = 0
+
+        for log in logs:
+            for key in ["money", "money_mugged", "pay", "cost"]:
+                if key not in log["data"]:
+                    continue
+                money += log["data"][key]
+
+        return money
+
+    def get_money_spent(self, **kwargs):
+        logs = self.get_logs(
+            log_category=LogCategories.MONEY_OUTGOING,
+            **kwargs
+        )
+
+        money = 0
+
+        for log in logs:
+            for key in ["money", "money_mugged", "cost", "total_cost", "upkeep_paid", "value", "bet"]:
+                if key not in log["data"]:
+                    continue
+                money += log["data"][key]
+
+        return money
+
+    def get_vault_deposits(self, **kwargs):
+        logs = self.get_logs(
+            LogTypes.VAULT_DEPOSIT,
+            **kwargs
+        )
+
+        return sum([l["data"]["deposited"] for l in logs])
+
+    def get_vault_withdrawals(self, **kwargs):
+        logs = self.get_logs(
+            LogTypes.VAULT_WITHDRAWAL,
+            **kwargs
+        )
+
+        return sum([l["data"]["withdrawn"] for l in logs])
+
+    def get_vault_net(self, **kwargs):
+        return self.get_vault_deposits(**kwargs) - self.get_vault_withdrawals(**kwargs)
--- torn_stats/views.py
+++ torn_stats/views.py
@@ -6,7 +6,7 @@ from .app import app, cache
 
 
 @app.route("/")
-@cache.cached(timeout=30)
+#@cache.cached(timeout=30)
 def display_info():
     start_date = datetime.utcnow().replace(day=1)
 
@@ -15,13 +15,27 @@ def display_info():
     for api_key in app.config["TORN_API_KEYS"]:
         client = TornClient(api_key)
 
+        logs = client.get_logs([
+                LogTypes.TRAVEL, LogTypes.VAULT_WITHDRAWAL, LogTypes.VAULT_DEPOSIT,
+                LogTypes.XANAX, LogTypes.CRIME, LogTypes.MISSION
+            ], start_date=start_date
+        )
+
+        def get_logs(log_type):
+            return [log for log in logs if log["log"] == log_type]
+
         profiles.append({
             "player": client.get_basic_info(),
-            "data": {
-                "Xanax Taken": len(client.get_logs(LogTypes.XANAX, start_date=start_date)),
-                "Crimes Committed": len(client.get_logs(LogTypes.CRIME, start_date=start_date)),
-                "Missions Completed": len(client.get_logs(LogTypes.MISSION, start_date=start_date)),
-            }
+            "xanax": len(get_logs(LogTypes.XANAX.value)),
+            "crimes": len(get_logs(LogTypes.CRIME.value)),
+            "missions": len(get_logs(LogTypes.MISSION.value)),
+            "travel": int(sum([l["data"]["duration"] for l in get_logs(LogTypes.TRAVEL.value)]) / 60),
+            "money_in": client.get_money_received(start_date=start_date),
+            "money_out": client.get_money_spent(start_date=start_date),
+            "vault": sum([
+                sum([l["data"]["deposited"] for l in get_logs(LogTypes.VAULT_DEPOSIT.value)]),
+                sum([l["data"]["withdrawn"] for l in get_logs(LogTypes.VAULT_WITHDRAWAL.value)]) * -1,
+            ]),
         })
 
     return render_template(
