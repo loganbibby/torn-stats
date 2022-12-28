cac6939736e0838a6a40f9594290b153cb66499e
Logan Bibby <logan@loganbibby.com>
2022-12-28 08:26:52 -0600

    Update client.py
    Fixed space->tab; added torn response caching

--- torn_stats/client.py
+++ torn_stats/client.py
@@ -1,168 +1,191 @@
 from enum import IntEnum
+from datetime import datetime
+import hashlib
 import requests
+from .app import cache
 
 
 class LogTypes(IntEnum):
-    XANAX = 2290
-    XANAX_OD = 2291
-    MISSION = 7815
-    CRIME = 5725
-    TRAVEL = 6000
-    GYM_DEFENSE = 5301
-    GYM_STRENGTH = 5300
-    GYM_DEXTERITY = 5303
-    GYM_SPEED = 5302
-    ATTACK_MUG = 8155
-    ATTACK_HOSPITALIZE = 8160
-    ATTACK_LEAVE = 8150
-    VAULT_DEPOSIT = 5850
-    VAULT_WITHDRAWAL = 5851
-    TRAVEL_BUY = 4201
-    COMPANY_PAY = 6221
-    COMPANY_TRAIN = 6264
-    MARKET_SELL = 1104
-    UPKEEP = 5920
+	XANAX = 2290
+	XANAX_OD = 2291
+	MISSION = 7815
+	CRIME = 5725
+	TRAVEL = 6000
+	GYM_DEFENSE = 5301
+	GYM_STRENGTH = 5300
+	GYM_DEXTERITY = 5303
+	GYM_SPEED = 5302
+	ATTACK_MUG = 8155
+	ATTACK_HOSPITALIZE = 8160
+	ATTACK_LEAVE = 8150
+	VAULT_DEPOSIT = 5850
+	VAULT_WITHDRAWAL = 5851
+	TRAVEL_BUY = 4201
+	COMPANY_PAY = 6221
+	COMPANY_TRAIN = 6264
+	MARKET_SELL = 1104
+	UPKEEP = 5920
 
 
 class LogCategories(IntEnum):
-    MONEY_INCOMING = 17
-    MONEY_OUTGOING = 14
+	MONEY_INCOMING = 17
+	MONEY_OUTGOING = 14
 
 
 class TornClient(object):
-    url = "https://api.torn.com"
+	url = "https://api.torn.com"
+	use_cache = True
 
-    def __init__(self, api_key):
-        self.api_key = api_key
+	def __init__(self, api_key):
+		self.api_key = api_key
 
-    def execute(self, endpoint, selection, **kwargs):
-        url = f"{self.url}/{endpoint}/"
+	def execute(self, endpoint, selection, **kwargs):
+		url = f"{self.url}/{endpoint}/"
 
-        query_params = {
-            "selections": selection,
-            "key": self.api_key
-        }
-        query_params.update(**kwargs)
+		query_params = {
+			"selections": selection,
+			"key": self.api_key
+		}
+		query_params.update(**kwargs)
+		
+		cache_key = self.get_cache_key(
+			url=url,
+			params=query_params
+		)
+		
+		payload = cache.get(cache_key)
+		
+		if not payload or not self.use_cache:
+			r = requests.get(url, params=query_params)
+			payload = r.json()
+			
+			if "to" in query_params and query_params["to"] > int(datetime.now().replace(hour=0, minute=0, second=0).timestamp()):
+				timeout = 60 * 60
+			else:
+				timeout = 60 * 60 * 24 * 7
+			
+			cache.set(cache_key, payload, timeout=timeout)
 
-        r = requests.get(url, params=query_params)
+		return payload
+	
+	def get_cache_key(self, **kwargs):
+		parts = [f"{str(key)}_{str(value)}" for key, value in kwargs.items()]
+		hash = hashlib.md5("__".join(parts).encode()).hexdigest()
+		return f"request_{self.api_key}_{hash}"
 
-        print(f"URL: {url} - {query_params}")
+	def get_basic_info(self):
+		return self.execute(
+			"user", "basic"
+		)
 
-        return r.json()
+	def get_logs(self, log_type=None, log_category=None, start_date=None, end_date=None, **kwargs):
+		if start_date:
+			kwargs["from"] = int(round(start_date.timestamp()))
 
-    def get_basic_info(self):
-        return self.execute(
-            "user", "basic"
-        )
+		if end_date:
+			kwargs["to"] = int(round(end_date.timestamp()))
 
-    def get_logs(self, log_type=None, log_category=None, start_date=None, end_date=None, **kwargs):
-        if start_date:
-            kwargs["from"] = int(round(start_date.timestamp()))
+		if log_type:
+			if isinstance(log_type, list):
+				kwargs["log"] = ",".join([str(t.value) for t in log_type])
+			else:
+				kwargs["log"] = int(log_type)
 
-        if end_date:
-            kwargs["to"] = int(round(end_date.timestamp()))
+		if log_category:
+			if isinstance(log_category, list):
+				kwargs["cat"] = ",".join([str(t.value) for t in log_category])
+			else:
+				kwargs["cat"] = int(log_category)
 
-        if log_type:
-            if isinstance(log_type, list):
-                kwargs["log"] = ",".join([str(t.value) for t in log_type])
-            else:
-                kwargs["log"] = int(log_type)
+		payload = self.execute(
+			"user", "log",
+			**kwargs
+		)["log"]
 
-        if log_category:
-            if isinstance(log_category, list):
-                kwargs["cat"] = ",".join([str(t.value) for t in log_category])
-            else:
-                kwargs["cat"] = int(log_category)
+		if not payload:
+			return []
 
-        payload = self.execute(
-            "user", "log",
-            **kwargs
-        )["log"]
+		return [v for k, v in payload.items()]
 
-        if not payload:
-            return []
+	def get_total_travel_time(self, start_date=None, end_date=None):
+		logs = self.get_logs(
+			LogTypes.TRAVEL,
+			start_date=start_date,
+			end_date=end_date
+		)
 
-        return [v for k, v in payload.items()]
+		return round(sum([l["data"]["duration"] for l in logs]) / 60, 0)
 
-    def get_total_travel_time(self, start_date=None, end_date=None):
-        logs = self.get_logs(
-            LogTypes.TRAVEL,
-            start_date=start_date,
-            end_date=end_date
-        )
+	def get_money_received(self, **kwargs):
+		logs = self.get_logs(
+			log_category=LogCategories.MONEY_INCOMING,
+			**kwargs
+		)
 
-        return round(sum([l["data"]["duration"] for l in logs]) / 60, 0)
+		money = 0
 
-    def get_money_received(self, **kwargs):
-        logs = self.get_logs(
-            log_category=LogCategories.MONEY_INCOMING,
-            **kwargs
-        )
+		for log in logs:
+			for key in ["money", "money_mugged", "pay", "cost"]:
+				if key not in log["data"]:
+					continue
+				money += log["data"][key]
 
-        money = 0
+		return money
 
-        for log in logs:
-            for key in ["money", "money_mugged", "pay", "cost"]:
-                if key not in log["data"]:
-                    continue
-                money += log["data"][key]
+	def get_money_spent(self, **kwargs):
+		logs = self.get_logs(
+			log_category=LogCategories.MONEY_OUTGOING,
+			**kwargs
+		)
 
-        return money
+		money = 0
 
-    def get_money_spent(self, **kwargs):
-        logs = self.get_logs(
-            log_category=LogCategories.MONEY_OUTGOING,
-            **kwargs
-        )
+		for log in logs:
+			for key in ["money", "money_mugged", "cost", "total_cost", "upkeep_paid", "value", "bet"]:
+				if key not in log["data"]:
+					continue
+				money += log["data"][key]
 
-        money = 0
+		return money
 
-        for log in logs:
-            for key in ["money", "money_mugged", "cost", "total_cost", "upkeep_paid", "value", "bet"]:
-                if key not in log["data"]:
-                    continue
-                money += log["data"][key]
+	def get_vault_deposits(self, **kwargs):
+		logs = self.get_logs(
+			LogTypes.VAULT_DEPOSIT,
+			**kwargs
+		)
 
-        return money
+		return sum([l["data"]["deposited"] for l in logs])
 
-    def get_vault_deposits(self, **kwargs):
-        logs = self.get_logs(
-            LogTypes.VAULT_DEPOSIT,
-            **kwargs
-        )
+	def get_vault_withdrawals(self, **kwargs):
+		logs = self.get_logs(
+			LogTypes.VAULT_WITHDRAWAL,
+			**kwargs
+		)
 
-        return sum([l["data"]["deposited"] for l in logs])
+		return sum([l["data"]["withdrawn"] for l in logs])
 
-    def get_vault_withdrawals(self, **kwargs):
-        logs = self.get_logs(
-            LogTypes.VAULT_WITHDRAWAL,
-            **kwargs
-        )
-
-        return sum([l["data"]["withdrawn"] for l in logs])
-
-    def get_vault_net(self, **kwargs):
-        return self.get_vault_deposits(**kwargs) - self.get_vault_withdrawals(**kwargs)
+	def get_vault_net(self, **kwargs):
+		return self.get_vault_deposits(**kwargs) - self.get_vault_withdrawals(**kwargs)
   
    # def get_upkeep(self, **kwargs):
-    #    logs = self.get_logs(
-     #       LogTypes.upkeep,
-      #      **kwargs
-       # return upkeep_paid(**kwargs)
-       # )
-        
-    def get_upkeep(self, **kwargs):
-        logs = self.get_logs(
-            log_category=LogCategories.MONEY_OUTGOING,
-            **kwargs
-        )
+	#    logs = self.get_logs(
+	 #       LogTypes.upkeep,
+	  #      **kwargs
+	   # return upkeep_paid(**kwargs)
+	   # )
+		
+	def get_upkeep(self, **kwargs):
+		logs = self.get_logs(
+			log_category=LogCategories.MONEY_OUTGOING,
+			**kwargs
+		)
 
-        money = 0
+		money = 0
 
-        for log in logs:
-            for key in ["upkeep_paid"]:
-                if key not in log["data"]:
-                    continue
-                money += log["data"][key]
+		for log in logs:
+			for key in ["upkeep_paid"]:
+				if key not in log["data"]:
+					continue
+				money += log["data"][key]
 
-        return money
+		return money
